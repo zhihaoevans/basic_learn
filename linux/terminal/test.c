2313411ac60b8f5fc3b7fc8acac5a432c3a403ea
zhihaoevans <1659500990@qq.com>
2023-08-22 11:39:26 +0800

    delete garbage and add new files

---
+++ linux/terminal/test.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+
+int main() {
+
+    int fd;
+    fd = open("/dev/tty0", O_RDWR);
+    if(fd < 0){
+        perror("open failed\n");
+    }
+    pid_t foreground_pgid = tcgetpgrp(fd);
+    if (foreground_pgid == -1) {
+        perror("tcgetpgrp");
+        return 1;
+    }
+
+    printf("Foreground process group ID: %d\n", foreground_pgid);
+
+    return 0;
+}
