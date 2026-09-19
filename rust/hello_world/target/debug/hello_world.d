3a5ac7334da7aa24fcf1d7d9db80e9b81cbf868f
xzh <xzh@XZHMacBookPro.local>
2026-09-20 01:06:05 +0800

    chore(rust): 移除被跟踪的构建产物
    - rust/main 为 rustc 编译产物，与源码 main.rs 重复
    - rust/hello_world/target/ 为 cargo 构建缓存，已被 **/target/ 规则忽略

--- rust/hello_world/target/debug/hello_world.d
+++
@@ -1,1 +0,0 @@
-/Users/xzh/code/learn/basic_learn/rust/hello_world/target/debug/hello_world: /Users/xzh/code/learn/basic_learn/rust/hello_world/src/main.rs
--- rust/hello_world/target/release/hello_world.d
+++
@@ -1,1 +0,0 @@
-/Users/xzh/code/learn/basic_learn/rust/hello_world/target/release/hello_world: /Users/xzh/code/learn/basic_learn/rust/hello_world/src/main.rs
